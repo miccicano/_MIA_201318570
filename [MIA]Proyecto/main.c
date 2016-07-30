8a9a7a1330ff1f06b6d1f78cf5ad3b27b1ea5d00
miccicano <miccicano@gmai.com>
2016-07-30 01:21:23 -0600

    .

--- [MIA]Proyecto/main.c
+++ [MIA]Proyecto/main.c
@@ -1,8 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <stdlib.h>
 
-int main()
+
+char com[1000];
+char com3[1000];
+int salida=0;
+int main(int argc, char *argv[])
 {
-    printf("Hello world!\n");
+
+     int i=0;
+
+    while (salida==0){
+        int i=0;
+    printf("\n Ingresar comando\n");
+   fgets(com,500,stdin);
+          for( i=0;i<1000;i++){
+              if(com[i] == '\n'){
+                  com[i] = '\0';
+              }else if(com[i]=='\\'){
+                  int ii=i+1;
+                  while(com[ii]!='\n'){
+                      com[ii]=' ';
+                      ii++;
+                  }
+                   com[ii]=' ';
+                fgets(com3,500,stdin);
+                strcat(com,com3);
+                printf(com);
+              }else{
+               com[i]= tolower(com[i]);
+              }
+          }
+//printf(com);
+       char *PalabraCadena = NULL;
+       PalabraCadena = strtok( com, " ");
+if(PalabraCadena!=NULL){
+       if(strcmp(PalabraCadena,"exit")==0 || strcmp(PalabraCadena,"Exit")==0 || strcmp(PalabraCadena,"EXIT")==0 ){
+                 salida=1;
+                 }
+}
+
+     if(PalabraCadena!=NULL){
+printf(PalabraCadena);}
+
+    }
+
+
     return 0;
 }
+
